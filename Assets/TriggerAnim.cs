2e6d5e8fecdd766f778bdc478da78f12cd615f66
Shade Desktop <gamingkendoka@gmail.com>
2021-03-17 01:18:01 +0100

    Door can flip! -optimisation issue
    I feel like the animator checking for states is taking up lots of machine power?
    Maybe I can optimize that in the future.

---
+++ Assets/TriggerAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnim : MonoBehaviour
+{
+    public Animator animator;
+    public BoxCollider otherCollider;
+    [Tooltip("type which local rotation the door must open; -x or x.")]
+    public string direction;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if u touch player
+        if (other.gameObject.CompareTag("Player")) {
+            animator.SetTrigger(direction);
+        }        
+    }
+    
+}

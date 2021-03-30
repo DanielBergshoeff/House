d6fd4f22e529e3b282bf996b35cd6a100273752e
Shade Desktop <gamingkendoka@gmail.com>
2021-03-30 12:40:12 +0200

    Made blue prefab, minor player jump tweaks

--- Assets/Scripts/TextureScrolling.cs
+++ Assets/Scripts/TextureScrolling.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour
+// u were busy making the material of cam bg scroll
+
+
+public class TextureScrolling : MonoBehaviour
 {
-    public Animator animator;
+
+    public Camera activeCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-           
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.LookAt(activeCamera.transform);
+       
     }
+
 }

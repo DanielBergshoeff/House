0ac288bb5c0470c9a725ea2b2408efd5d68dcad4
Shade Desktop <gamingkendoka@gmail.com>
2021-04-01 16:17:24 +0200

    Made a ObjectChildConcealer
    For when players enter a room that needs roof to filter out background visual noise

---
+++ Assets/Scripts/ChildConcealer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//reveal the childed objects in a fade when player enters the collider
+
+public class ChildConcealer : MonoBehaviour
+{
+    private int _amountOfChildren;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        //check if its the player
+        if (other.CompareTag("Player"))
+        {
+            RevealChildren();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //check if its the player
+        if (other.CompareTag("Player"))
+        {
+            HideChildren();
+        }
+    }
+
+    void RevealChildren()
+    {
+        CountChildren();
+        //use that number to cycle through and affect children
+        for (int i = 0; i < _amountOfChildren; i++)
+        {transform.GetChild(i).gameObject.SetActive(true);}//enable
+    }
+
+    void HideChildren() {
+        CountChildren();
+        for (int i = 0; i < _amountOfChildren; i++)
+        {transform.GetChild(i).gameObject.SetActive(false);}//disable
+    }
+
+    void CountChildren() {
+        //learn amount of children on this Parent
+        _amountOfChildren = transform.childCount;}
+}

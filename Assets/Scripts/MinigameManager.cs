23cab1915c8b92c7acf07c846b90add0ef927d00
Shade Desktop <gamingkendoka@gmail.com>
2021-04-11 19:53:41 +0200

    Minigame wip
    - made healthbar according to Brackeys tutorial
    - fancy UML available too
    - wip bookmark at CalcHP(){}

---
+++ Assets/Scripts/MinigameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//this is where I define the amount of penalty points or HP there are.
+
+public class MinigameManager : MonoBehaviour
+{
+    public Slider slider;
+
+    public int health = 10;
+    public float badCommentValue = 1f;
+    public float goodCommentValue = 1f;
+    public float ruminateValue = 2;
+    public float counterValue = 2;
+
+    public void Awake() {
+        //this makes sure you start the minigame at max health
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void CalcHP(int value) { //recalculate health using outcome of a comment
+        health += value;
+        Debug.Log("Lost/gained " + value + " health.");
+        //update the health bar accordingly
+        SetHeath();
+    }
+
+    void SetHeath() {
+        //sets the value or referenced slider to current health
+        slider.value = health;
+    }
+}
---
+++ Assets/Scripts/StreamComments.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; //do we need this? (optimizse)
+
+
+public class StreamComments : MonoBehaviour
+{
+    //create a reference to the minigame manager to allow conversation between scripts
+    public MinigameManager minigameManager;
+
+    public enum Nature {Good, Bad, Counter, Ruminate, Neutral};
+    [Tooltip("Set the nature of this comment. Is it a good or bad one, or does it either an indication of a counter to another sentence or rumination?")]
+    public Nature commentNature;
+    //optimisation tip: can we prevent every sentence having to make a new enum set?
+
+    [Tooltip("Set how long the comment will appear on screen for.")]
+    public float lifetime = 4;
+
+    private void Update()
+    {
+        //count down time so that the comment will disappear at some point 
+        lifetime -= 1 * Time.deltaTime;
+
+        DoThing();
+        
+    }
+
+    //int DoThing() {
+    //    if (lifetime <= 0)
+    //    {
+    //        if (commentNature == Nature.Bad)
+    //        {
+    //            minigameManager.CalcHP(5);
+    //        }
+    //    }
+    //}
+}

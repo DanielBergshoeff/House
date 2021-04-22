441fa7561544037df36342290786786f6e28898e
Shade Desktop <gamingkendoka@gmail.com>
2021-04-22 13:05:28 +0200

    More excessive subtraction solved!
    Look at me go B)

--- Assets/Scripts/StreamHealth.cs
+++ Assets/Scripts/StreamHealth.cs
@@ -13,8 +13,8 @@ public class StreamHealth : MonoBehaviour
 
 
     private int _phasePoints; //the total points that will be added to the total health at the end of a round
-
     public void Subtract(int value) {
+        Debug.Log("Gonna subtract " + value);
         health -= value;
     }
 
@@ -24,18 +24,37 @@ public class StreamHealth : MonoBehaviour
 
     public void CalcHP()
     {
-        //check viewers comments for bad comments
+        
         int childCountB = minigameManager.viewerComments.transform.childCount;
         for (int i = 0; i < childCountB; i++)
         {
-            if (minigameManager.viewerComments.transform.GetChild(i).GetComponent<StreamButton>().buttonComment.nature == Comment.Nature.Bad) {
+            //subtract viewers bad comments
+            if (minigameManager.viewerComments.transform.GetChild(i).GetComponent<StreamButton>().buttonComment.nature == Comment.Nature.Bad)
+            {
                 Subtract(minigameManager.badCommentValue);
-                Debug.Log("Viewer comment" + minigameManager.viewerComments.transform.GetChild(i).GetComponent<StreamButton>().buttonComment.Text + " was bad");
+                Debug.Log("a viewer comment was bad");
             }
         }
 
-        
+        //subtract yellow bad comments
+        if (minigameManager.yellowChild1.transform.GetComponentInChildren<StreamButton>().buttonComment.nature == Comment.Nature.Bad) {
+          Debug.Log("yellow child 1 is bad");
+          Subtract(minigameManager.badCommentValue);
+        }
+        if (minigameManager.yellowChild2.transform.GetComponentInChildren<StreamButton>().buttonComment.nature == Comment.Nature.Bad) {
+          Debug.Log("yellow child 2 us bad");
+          Subtract(minigameManager.badCommentValue);
+        }
 
+        //subtract yellow  ruminations
+        if (minigameManager.yellowChild1.transform.GetComponentInChildren<StreamButton>().buttonComment.nature == Comment.Nature.Ruminate) {
+            Debug.Log("yellow child 1 is ruminate");
+            Subtract(minigameManager.ruminateValue);
+        }
+        if (minigameManager.yellowChild2.transform.GetComponentInChildren<StreamButton>().buttonComment.nature == Comment.Nature.Ruminate) {
+            Debug.Log("yellow child 2 is ruminate");
+            Subtract(minigameManager.ruminateValue);
+        }
 
         //health += _phasePoints;
         //sets the value or referenced slider to current health

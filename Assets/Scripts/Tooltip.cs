30c72195c059c4b8fa5d6e2876b5482241b99904
Shade Desktop <gamingkendoka@gmail.com>
2021-04-28 01:23:27 +0200

    WIP tooltip

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//InputSystemUIInputModule inputModule;
+public class Tooltip : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Vector2 position = Input.mousePosition;
+        transform.position = position; //then set the pos of tooltip base off mouse pos
+    }
+}
---
+++ Assets/Scripts/TooltipSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//most code taken from Game Dev Guid at: https://www.youtube.com/watch?v=HXFoUGw7eKk
+public class TooltipSystem : MonoBehaviour
+{
+    public static TooltipSystem current;
+    public Tooltip tooltip;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    public static void Show() {
+        current.tooltip.gameObject.SetActive(true);
+    }
+
+    public static void Hide() {
+        current.tooltip.gameObject.SetActive(false);
+    }
+
+}
+

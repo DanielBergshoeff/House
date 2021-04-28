da90fdf5ca6cf4a0585def68d9b467f4dd388baa
Shade Desktop <gamingkendoka@gmail.com>
2021-04-28 02:20:05 +0200

    Getting too complicated, will revert in a moment

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.UI;//well this was prod and poked 7:28
 
+//code used from samyam at: https://www.youtube.com/watch?v=H2RLv0aWUB0
+
 //InputSystemUIInputModule inputModule;
 public class Tooltip : MonoBehaviour
 {
-    InputSystemUIInputModule inputModule;
-    public Input inputThing;
+    //InputSystemUIInputModule inputModule;
     // Update is called once per frame
+       
+    //reference your action map so u can talk about their items
+    private PlayerInput controls;
+
+
+    private void Awake() {
+        controls = new PlayerInput(); //this instantiates controls apparently
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
     void Update()
     {
-        //reference your action map so u can talk about their items
-        //private PlayerActions controls;
-        //vector2 mouseposition = controls.player
-        //vector2 position = input.mouseposition;
-        //transform.position = position; //then set the pos of tooltip base off mouse pos
+    //vector2 mouseposition = controls.player
+    //vector2 position = input.mouseposition;
+    //transform.position = position; //then set the pos of tooltip base off mouse pos
     }
 }

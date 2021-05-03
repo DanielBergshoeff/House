84aa3e98b69851125d884a3e4db3328fb1b5cbd4
Shade Desktop <gamingkendoka@gmail.com>
2021-05-04 01:37:03 +0200

    Quit game enabled

--- Assets/Scripts/TalkTo.cs
+++ Assets/Scripts/TalkTo.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+//put this script on any NPC or object through which you want to use fungus dialogue trees (through prefabs plz)
 
 public class TalkTo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputActionAsset playerControls;
+    bool isTalking = false; //check if youre talking or not
+
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+                Debug.Log("Collided with player");
+            if (isTalking == false && Input.GetKeyDown("e"))
+            {
+                isTalking = true;
+                Debug.Log("Pressed something");
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }

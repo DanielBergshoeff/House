edc11343353ed27a07a4d8b689396603305bc037
Shade Desktop <gamingkendoka@gmail.com>
2021-05-04 12:53:12 +0200

    Talk to usability
    - made it so that the conversation starts automatically when you enter a trigger sphere around an NPC
    - and when you exit it, it will end the flowcharts blocks, and destroy the menu buttons it might have made appear in the meantime

--- Assets/Scripts/TalkTo.cs
+++ Assets/Scripts/TalkTo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Fungus;
 
 //put this script on any NPC or object through which you want to use fungus dialogue trees (through prefabs plz)
 
@@ -10,18 +11,28 @@ public class TalkTo : MonoBehaviour
     public InputActionAsset playerControls;
     bool isTalking = false; //check if youre talking or not
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-                Debug.Log("Collided with player");
-            if (isTalking == false && Input.GetKeyDown("e"))
-            {
-                isTalking = true;
-                Debug.Log("Pressed something");
-            }
+            Debug.Log("Collided with player");
+
+            GetComponentInChildren<Flowchart>().ExecuteBlock("Start");
+            //if (isTalking == false && Input.GetKeyDown("e"))
+            //{
+            //    isTalking = true;
+            //    Debug.Log("Pressed something");
+            //}
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player") {
+            GetComponentInChildren<Flowchart>().StopAllBlocks(); //make sure the same flowchart cannot happen twice
+            Destroy(GameObject.Find("MenuDialog")); //destroy the menu in hierarchy to prevent the menus from staying in view when walking out of the talk zone of this scripts parent
+        }
+        Debug.Log("Exited NPC talk trigger collider");
+    }
 
 }

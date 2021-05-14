d48c6181f64e17cb238f6463f1bd3f8e05307442
Shade Desktop <gamingkendoka@gmail.com>
2021-05-14 08:34:00 +0200

    Set stream cursor via script
    - plus some tiny stream comments tweaks

---
+++ Assets/Scripts/ChangeCursor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    public Texture2D streamCursor;
+
+    void Start()
+    {
+        Cursor.SetCursor(streamCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+}

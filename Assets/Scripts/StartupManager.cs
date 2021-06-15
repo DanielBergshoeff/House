bf595a1df551a3411418f461e7c203368dd33cba
Shade Desktop <gamingkendoka@gmail.com>
2021-06-15 20:51:25 +0200

    Visuals work, not yet audio

---
+++ Assets/Scripts/StartupManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//put this on the parent (canvas) holding the ui elements for the startup screen
+public class StartupManager : MonoBehaviour
+{
+    public GameObject player;
+    public AudioClip mainTheme;
+
+    private void Update()
+    {
+        if (Input.anyKey) {
+            CloseTitle();
+        }
+    }
+
+    void CloseTitle() {
+        //change music to main theme
+        player.GetComponent<AudioSource>().clip = mainTheme;
+        player.GetComponent<AudioSource>().loop = true;
+
+
+        //initiate fade out anim
+        GetComponent<Animator>().SetTrigger("fade");
+    }
+}

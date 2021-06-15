a9aa85c1f941d1b5e4ab505035a6bbe4d3585e17
Shade Desktop <gamingkendoka@gmail.com>
2021-06-15 21:06:25 +0200

    Intro complete
    smooth as butter

--- Assets/Scripts/StartupManager.cs
+++ Assets/Scripts/StartupManager.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 //put this on the parent (canvas) holding the ui elements for the startup screen
 public class StartupManager : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject mainAudioPlayer;
     public AudioClip mainTheme;
+
+    private AudioSource _anim;
+
+    private void Awake()
+    {
+        _anim = mainAudioPlayer.GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
-        if (Input.anyKey) {
+        //make sure the game only starts playing when player uses a key. we check if an animation isnt already playing, else youd interrup whatever sound mainAudioPlayer is playing for main Theme
+        if (Input.anyKey && _anim.isPlaying == false) {
             CloseTitle();
         }
     }
 
     void CloseTitle() {
         //change music to main theme
-        player.GetComponent<AudioSource>().clip = mainTheme;
-        player.GetComponent<AudioSource>().loop = true;
+        _anim.clip = mainTheme;
+        _anim.loop = true;
+        _anim.Play();
 
 
         //initiate fade out anim
         GetComponent<Animator>().SetTrigger("fade");
+        GetComponent<StartupManager>().enabled = false; //shut this off so script wont keep doing update
     }
 }
